d0c4a5ad428e0dd7fd6dc042fc70390747227125
Phillip Tran <ptrandev@protonmail.com>
2025-03-27 12:23:05 -0400

    new model; added accuracy module

---
+++ src/accuracy.py
@@ -0,0 +1,53 @@
+#
+# Accuracy Metrics Module
+#
+# This module is responsible for evaluating the accuracy of the trained model
+# using the test dataset.
+#
+
+import os
+import tensorflow as tf
+from emnist import extract_test_samples
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.output import LABELS
+
+# get the accuracy, loss, and confusion matrix
+def evaluate_model(model_path):
+    # Load the test dataset
+    test_images, test_labels = extract_test_samples("letters")
+
+    # Normalize the images to a pixel range of 0 to 1
+    test_images = test_images / 255.0
+
+    # Load the trained model
+    model = tf.keras.models.load_model(model_path)
+
+    # Evaluate the model, get the cross-entropy loss and accuracy
+    loss, accuracy = model.evaluate(test_images, test_labels, verbose=0)
+
+    # get the confusion matrix
+    predictions = model.predict(test_images)
+    confusion_matrix = tf.math.confusion_matrix(test_labels, tf.argmax(predictions, axis=1))
+
+    # calculate the accuracy of each class, return as a dictionary with class labels
+    # discard first class in confusion matrix as it is not present in the dataset
+    class_accuracy = {}
+    for i in range(1, len(LABELS) + 1):
+        class_accuracy[LABELS[i - 1]] = (confusion_matrix[i][i] / tf.reduce_sum(confusion_matrix[i])).numpy().item()
+
+    # get the highest and lowest accuracy classes and their accuracy
+    highest_accuracy = max(class_accuracy, key=class_accuracy.get)
+    lowest_accuracy = min(class_accuracy, key=class_accuracy.get)
+
+    return {
+        "loss": loss,
+        "accuracy": accuracy,
+        "confusion_matrix": confusion_matrix,
+        "class_accuracy": class_accuracy,
+        "highest_accuracy": highest_accuracy,
+        "lowest_accuracy": lowest_accuracy
+    }
+
+print(evaluate_model(os.path.join(os.path.dirname(__file__), "model.keras")))
