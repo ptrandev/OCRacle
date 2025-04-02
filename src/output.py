f0cef2a010d8bce720bccb6b4d721e4a28c0d400
Phillip Tran <ptrandev@protonmail.com>
2025-04-02 13:20:14 -0400

    Update output.py

--- src/output.py
+++ src/output.py
@@ -13,8 +13,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.model import predict
 import numpy as np
 
-from typing import Union, Dict
-
 LABELS = [
     "A",
     "B",
